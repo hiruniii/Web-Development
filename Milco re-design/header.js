05d57ee195b73e6320d1a45cd53bd81a8d245d7d
Hiruni <hiruni.nd@gmail.com>
2022-04-20 17:36:47 +0530

    added card content and footer

--- Milco re-design/header.js
+++ Milco re-design/header.js
@@ -46,9 +46,56 @@ class Footer extends HTMLElement{
 
   connectedCallback() {
     this.innerHTML = `
-    
+    <footer class="footer">
+    <div></div>
+
+    <div></div>
+
+    <div class="contact">
+      <table>
+        <tr>
+          <td>
+              <a target="_blank" href="https://www.facebook.com/milcohighland/">
+                <img src="icons8-facebook.png" />
+              </a>
+          </td>
+          <td>@milcohighland</td>
+        </tr>
+
+        <tr>
+          <td>
+            <a href="mailto:info@milco.lk">
+              <img src="icons8-mail.png" class="footer-icon" />
+            </a>
+          </td>
+          <td>info@milco.lk</td>
+        </tr>
+
+        <tr>
+          <td><img src="icons8-phone.png" class="footer-icon" /></td>
+          <td>(+94) 112 582331-4</td>
+        </tr>
+
+        <tr>
+          <td><img src="icons8-fax.png" class="footer-icon" style="margin-right: 1rem" /></td>
+          <td>(+94) 112 368082<br />
+            (+94) 115 333328
+          </td>
+        </tr>
+
+        <tr>
+          <td><img src="icons8-house.png" class="footer-icon" style="margin-right: 1rem" /></td>
+          <td>Milco (Pvt) Ltd, No: 45, Nawala Road,Narahenpita Colombo 05, Sri Lanka
+          </td>
+        </tr>
+
+      </table>
+    </div>
+
+  </footer>
     `;
   }
 }
 
 customElements.define("header-component", Header);
+customElements.define("footer-component", Footer);
